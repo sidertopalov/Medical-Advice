6ddece8540b7ab06dfc0a6a29ec201595a9ee751
Sider Topalov <sidertopalov@gmail.com>
2016-07-19 15:34:54 +0300

    TestController

--- App/Controllers/Test/TestController.php
+++ App/Controllers/Test/TestController.php
@@ -15,22 +15,8 @@ class TestController extends Controller {
         /** @var Yee\Yee $yee */
         $app = $this->getYee();
 
-//         // $data = array(
-//         //     );
-
-//      	//  $mailer = new Mailer( "sidertopalov@gmail.com", "cansua.ali@gmail.com", "signup", $data, "Activate Account 2" );
-
-//     	// $mailer->buildEmail()->sendEmail();	
-
-//         // $hashEmail = md5("sidertopalov@gmail.com");
-//         // $normalEmail = "sidertopalov@gmail.com";
-
-//         // if ($hashEmail == md5($normalEmail)) {
-//         //     echo "Yes";
-//         // }
-//         // echo "<br>";
-//         // echo sha1("sidertopalov@gmail.com");
+     //    $data = array(
+     //        );
 
     }
-
 }
